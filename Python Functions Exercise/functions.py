6bb6f53d72a82935e2253ff9fe84802b3c046bc0
Marcin Fiuk <fiukmarcinn@gmail.com>
2026-02-14 19:24:57 +0000

    Ex 6-10

--- Python Functions Exercise/functions.py
+++ Python Functions Exercise/functions.py
@@ -55,7 +55,7 @@ showEmployee("Jessa")
 #   Inside this outer function, define an inner function that calculates the sum of a and b.
 #   he outer function should then add 5 to this sum.
 #   Finally, the outer function should return the resulting value.
-
+'''
 def outer_func(a,b):
   def inner_func(a,b):
     return a+b
@@ -63,3 +63,55 @@ def outer_func(a,b):
   return sum +5 
 
 print(outer_func(3,3))
+'''
+
+# Exercise 6: Create a recursive function
+# Write a program to create a recursive function that calculates the sum of numbers from 0 to 10.
+'''
+def sum_range(n):
+  if n==0:
+    return 0
+  else:
+    return n + sum_range(n-1)
+
+print(sum_range(10))
+'''
+
+# Exercise 7: Assign a different name to function and call it through the new name
+# Below is the function display_student(name, age). Assign a new name show_student(name, age) to it and call it using the new name.
+'''
+def display_student(name, age):
+    print(name, age)
+
+show_student = display_student
+
+display_student("Emma", 26)
+show_student("Emma", 26)
+'''
+
+# Exercise 8: Generate a Python list of all the even numbers between 4 to 30
+'''
+def generate_even_numbers_from_range(start,stop):
+    print(list(range(start,stop,2)))
+
+generate_even_numbers_from_range(4,30)
+'''
+
+# Exercise 9: Find the largest item from list
+'''
+l1 = [4, 6, 8, 24, 12, 2]
+
+def find_max(list):
+  print(max(list))
+
+find_max(l1)
+'''
+
+# Exercise 10: Call Function using both positional and keyword arguments
+# Define a function describe_pet(animal_type, pet_name) that prints a description of a pet. Call this function using both positional and keyword arguments.
+
+def describe_pet(animal_type, pet_name):
+  print(animal_type, pet_name)
+
+describe_pet('dog', 'Bingo')
+describe_pet(animal_type='dog',pet_name='Bingo')
